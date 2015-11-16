6c59250abfc7980dca37fe874446dec0db8880f3
Tristan JARRY <E113694F@S046V7pc08.ensinfo.sciences.univ-nantes.prive>
2015-11-16 16:02:49 +0100

    Add new classes

---
+++ TrialADLM2/src/com/alma/csa/hadl/m2/component/Port.java
@@ -0,0 +1,7 @@
+package com.alma.csa.hadl.m2.component;
+
+import fr.univnantes.alma.hadl.m2.service.Interface;
+
+public class Port extends Interface {
+
+}
---
+++ TrialADLM2/src/fr/univnantes/alma/hadl/m2/service/RequiredService.java
@@ -0,0 +1,5 @@
+package fr.univnantes.alma.hadl.m2.service;
+
+public class RequiredService {
+
+}
