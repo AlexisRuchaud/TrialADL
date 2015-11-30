5ca14ae76a63f8a933a4c4f635801ef6dcfc3de8
AlexisRuchaud <alexruchaud@gmail.com>
2015-11-30 17:33:01 +0100

    ajout roles M1

---
+++ TrialADL/src/M1/Roles/RoleCalled.java
@@ -0,0 +1,12 @@
+package M1.Roles;
+
+import M2.connector.CalledRole;
+
+public class RoleCalled extends CalledRole {
+
+	public RoleCalled(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ TrialADL/src/M1/Roles/RoleCaller.java
@@ -0,0 +1,12 @@
+package M1.Roles;
+
+import M2.connector.CallerRole;
+
+public class RoleCaller extends CallerRole {
+
+	public RoleCaller(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
